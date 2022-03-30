cdff15719e7c066d6cc4c465dbc6751b5c856b5c
Anmol Brar <brarsanmol@protonmail.com>
2022-03-30 01:26:00 -0400

    feat: create verify command
    - create e-mail regex pattern for mcgill students
    - add necessary intents (all of them for now)
    - provide basic response's on the verify command on whether the input
      entered is valid.

---
+++ sentinel/commands/verify.py
@@ -0,0 +1,27 @@
+import re
+
+import discord
+from discord import app_commands
+
+EMAIL_PATTERN = re.compile(r"[a-zA-Z]+\.[a-zA-Z]+@mail\.mcgill\.ca")
+
+
+def is_direct_message_channel():
+    def predicate(interaction: discord.Interaction) -> bool:
+        return interaction.channel.type == discord.ChannelType.private
+
+    return app_commands.check(predicate)
+
+
+@app_commands.command()
+@app_commands.describe(email_address="Your @mail.mcgill.ca e-mail address.")
+@is_direct_message_channel()
+async def verify(interaction: discord.Interaction, email_address: str) -> None:
+    if EMAIL_PATTERN.match(email_address, re.IGNORECASE):
+        await interaction.response.send_message(
+            "The e-mail address provided is valid.", ephemeral=True
+        )
+    else:
+        await interaction.response.send_message(
+            "The e-mail address provided is invalid.", ephemeral=True
+        )
--- sentinel/main.py
+++ sentinel/main.py
@@ -1,17 +1,26 @@
 import configparser
 import os
+import re
 
 import discord
+from discord import app_commands
+
+from sentinel.commands.verify import verify
+
+EMAIL_PATTERN = re.compile(r"[a-zA-Z]+\\.[a-zA-Z]+@mail\\.mcgill\\.ca")
 
 
 class Sentinel(discord.Client):
     def __init__(self, **options):
-        super().__init__(loop=None, **options)
+        super().__init__(loop=None, intents=discord.Intents().all())
 
         self.configuration = configparser.ConfigParser()
         self.configuration.read(
             os.path.join(os.path.dirname(__file__), "configuration.ini")
         )
 
+        self.tree = app_commands.CommandTree(self)
+
     async def on_ready(self):
-        pass
+        self.tree.add_command(verify)
+        await self.tree.sync()
